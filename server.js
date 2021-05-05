dc5095037bbdcfa918ffffeb7fd7c4549dbed836
sunil kumar mutaka <carbojet@gmail.com>
2021-05-05 19:40:38 +0530

    app modified

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.prepare().then(() => {
     ctx.res.statusCode = 200;
   });
   server.listen(port, () => {
-    //console.log(`> Ready on http://localhost:${port}`);
+    console.log(`> Ready on http://localhost:${port}`);
   });
   
 });
