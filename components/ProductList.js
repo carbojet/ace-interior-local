a2a799ab1591d23ac948f860c4997b80fe8a3225
sunil kumar mutaka <carbojet@gmail.com>
2021-05-11 15:18:56 +0530

    add product list comp

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,6 +1,6 @@
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
-import { Card, ResourceList, Stack, Thumbnail,Heading } from '@shopify/polaris';
+import { Card, ResourceList,ResourceItem, Stack, Thumbnail,Heading } from '@shopify/polaris';
 
 
 const GET_FIRST_PRODUCTS = gql`
@@ -38,23 +38,55 @@ query getProducts($row:Int!){
 `;
 
 function ProductList (){
-
+ 
     const {loading, error, data} = useQuery(GET_FIRST_PRODUCTS, { variables: { row: 20 } });
     if (loading) return <div>loading...</div>
     if (error) return <div>{error.message}</div>
-    const products = data.products.edges;
-
-    const resourceName = {
-        singular: 'Product',
-        plural: 'Products',
-    };
-    
     //console.log('stored products',data.products.edges);    
     return(
         <Card>
-            
-            
+          <ResourceList
+            items={data.products.edges}
+            renderItem={ item => {
+              const product = item.node;
+              const price = product.variants.edges[0].node.price;
+              const media = (
+                  <Thumbnail 
+                      source={
+                          product.images.edges[0] ? product.images.edges[0].node.originalSrc :''
+                      }
+                      alt={
+                          product.images.edges[0] ? product.images.edges[0].node.altText : ''
+                      }
+                  />
+              );
+              return(
+                <ResourceList.Item
+                    id={product.id}
+                    media={media}
+                    url={'product/'+product.id}
+                    accessibilityLabel={`view Details for ${product.title}`}
+                >
+                  <Stack>
+                      <Stack.Item fill>
+                          <Heading>
+                            {product.title}
+                          </Heading>
+                      </Stack.Item>
+                      <Stack.Item>
+                          <p>INR {price}</p>
+                      </Stack.Item>
+                      <Stack.Item>
+                          <p>{ product.totalInventory}</p>
+                      </Stack.Item>
+                  </Stack>
+                </ResourceList.Item>
+              )
+            }}
+          >
+          </ResourceList>
         </Card>
     )
+    
 }
 export default ProductList;
