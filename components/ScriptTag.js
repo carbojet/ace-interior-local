029ab6e506a7e33905ad3cdeab57298a2c109dc1
sunil kumar mutaka <carbojet@gmail.com>
2021-05-13 17:42:16 +0530

    add query for script tag

--- components/ScriptTag.js
+++ components/ScriptTag.js
@@ -1,7 +1,22 @@
 import gql from 'graphql-tag';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 
+const GET_SCRIPT_TAG = gql `
+  query{
+    scriptTags(first:5){
+      edges{
+        node{
+          id
+          src
+          displayScope
+        }
+      }
+    }
+  }
+`;
+
 function ScriptTag(){
+
   return(
     <p>test</p>
   )
