bf0623d0f6203fb50043e1903701973053f8b586
sunil kumar mutaka <carbojet@gmail.com>
2021-05-13 17:36:18 +0530

    script tag

--- components/ScriptTag.js
+++ components/ScriptTag.js
@@ -1,22 +1,7 @@
 import gql from 'graphql-tag';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 
-const GET_SCRIPT_TAG = gql`
-  query{
-    scriptTags(first:1){
-      edges{
-        node{
-          id
-          src
-          displayScope
-        }
-      }
-    }
-  }
-`;
-
 function ScriptTag(){
-  const [loading,error,data] = useQuery(GET_SCRIPT_TAG)
   return(
     <p>test</p>
   )
