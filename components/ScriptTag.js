d21c35be10be191626aff1883d9060ec2869116a
sunil kumar mutaka <carbojet@gmail.com>
2021-05-13 17:03:48 +0530

    script get and add query

--- components/ScriptTag.js
+++ components/ScriptTag.js
@@ -1,6 +1,19 @@
 import gql from 'graphql-tag';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 
+const GET_SCRIPT_TAG = gql`
+  query{
+    scriptTags(first:1){
+      edges{
+        node{
+          id
+          src
+          displayScope
+        }
+      }
+    }
+  }
+`;
 
 const CREATE_SCRIPT_TAG = gql`
   mutation scriptTagCreate($input : ScriptTagInput!){
@@ -16,9 +29,10 @@ const CREATE_SCRIPT_TAG = gql`
   }
 `;
 
-function ScriptTag(){        
-   return(
-     <p>this is script to create new for first time app installed</p>
-   ) 
+function ScriptTag(){
+  const [loading,error,data] = useQuery(GET_SCRIPT_TAG)
+  if(!loading){
+    console.log(data)
+  }
 }
 export default ScriptTag;
